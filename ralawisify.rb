7b094f4c55b0e3ada28aadbae2cde9251689aaac
Charles J Hardy <chuckjhardy@gmail.com>
2014-05-31 20:00:48 +0100

    Add Row Class

--- ralawisify.rb
+++ ralawisify.rb
@@ -1,4 +1,5 @@
 require 'csv'
+require_relative 'ralawisify/shopify/row'
 require_relative 'ralawisify/shopify'
 
 class Ralawisify
--- ralawisify/shopify.rb
+++ ralawisify/shopify.rb
@@ -49,56 +49,12 @@ class Ralawisify
 
     private
 
-    def formatted_handle
-      title.map(&:downcase).join('-').gsub(' ', '-')
-    end
-
-    def image_alt
-      title.join(' ')
-    end
-
-    def title
-      [ @rows.first['Brand Name'], @rows.first['Name'] ]
-    end
-
-    def published?
-      'FALSE'
-    end
-
-    def shipping?
-      'TRUE'
-    end
-
-    def taxable?
-      'TRUE'
-    end
-
-    def fulfillment
-      'manual'
-    end
-
-    def inventory_policy
-      'deny'
-    end
-
-    def inventory_quantity
-      1
-    end
-
-    def image
-      'http://www.promotional-store.com/images/thumbs/' + @rows.first['LifeStylePic']
-    end
-
-    def tags
-      @rows.map { |x| x['Subcategory'] }.uniq.join(', ')
-    end
-
     def row
       ->(acc, (k,v)) { acc.merge({ k => value_for(v) }) }
     end
 
     def value_for(value)
-      send(value)
+      Row.new(@rows).public_send(value)
     rescue NoMethodError, TypeError
       @rows.first[value]
     end
